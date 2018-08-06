a3eb69c6024b893232e79b2a604f8803ff79dc24
Moshe Raab <moshe.raab@gmail.com>
2018-08-06 07:57:06 +0300

    Adding GET request

--- server/server.js
+++ server/server.js
@@ -29,6 +29,15 @@ app.post('/todos', (req, res) => {
 	});
 });
 
+// get todos
+app.get('/todos', (req, res) => {
+	Todo.find().then( (todos) => {
+		res.send({todos});
+	}, (e) => {
+		res.status(400).send(e);
+	});
+});
+
 app.listen(3000, () => {
 	console.log('Started server on port 3000');
 });
