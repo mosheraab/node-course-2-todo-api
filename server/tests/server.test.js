c7fbff628941a5baa80fa9a4456e441f6cf19b78
Moshe Raab <moshe.raab@gmail.com>
2018-08-06 08:29:51 +0300

    Adding test for GET

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,17 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+		text: "Something to do"
+	}, {
+		text: "Something to do"
+	}
+];
+
 beforeEach( (done) => {
-	Todo.remove({}).then( () => done());
+	Todo.remove({}).then( () => {
+		return Todo.insertMany(todos);
+		}).then( () => done());
 });
 
 describe('POST /todos', () => {
@@ -22,13 +31,13 @@ describe('POST /todos', () => {
 				if (err) {
 					return done(err);
 				}
-				Todo.find().then( (todos) => {
+				Todo.find({text: text}).then( (todos) => {
 					expect(todos.length).toBe(1);
-					expect(todos[0].text).toBe(text);
+					//expect(todos[0].text).toBe(text);
 					done();
 				}). catch( (e) => done(e) )
 			})
-	});
+	}); 
 
 	it('Test - not creating todo', (done) => {
 		var text = '';
@@ -44,13 +53,23 @@ describe('POST /todos', () => {
 					return done(err);
 				}
 				Todo.find().then( (todos) => {
-					expect(todos.length).toBe(0);
+					expect(todos.length).toBe(2);
 					//expect(todos[0].text).toBe(text);
 					done();
 				}). catch( (e) => done(e) )
 			})
 	});
-
-	
 });
 
+describe('GET /todos', () => {
+	it('Test - Getting All Todos', (done) => {
+		request(app)
+			.get('/todos')
+			.expect(200)
+			.expect((res) => {
+				// console.log('Todos: ', JSON.stringify(res.body, undefined, 2));
+				expect(res.body.todos.length).toBe(2);
+			})
+			.end(done);
+	});
+});
